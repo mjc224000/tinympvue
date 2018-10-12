f101b2e7cd9485a49887c333e2d94a0ab7bbdace
Administrator <397938635@qq.com>
2018-10-12 17:45:38 +0800

    init commite

--- server/index.js
+++ server/index.js
@@ -1,13 +1,12 @@
 const express = require('express');
+const request=require('request');
 const msgManager = require('./msg_manager');
 const WebSocket = require('ws');
 const options = require('./option');
-for(var key in options){
-  console.log(options);
-}
 let app = express();
 app.get('/', function (req, res) {
-  res.send('nmsl')
+  console.log(req.query);
+  res.send('丛后台传过来的东西')
 })
 var https = require('https');
 let server = https.createServer(options, app);
