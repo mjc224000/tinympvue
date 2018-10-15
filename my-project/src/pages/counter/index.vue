bc708585ac9a714c0cc1d00f795da6606dfabd8f
Administrator <397938635@qq.com>
2018-10-15 14:04:44 +0800

    init commite

--- my-project/src/pages/counter/index.vue
+++ my-project/src/pages/counter/index.vue
@@ -1,46 +1,62 @@
 <template>
   <div class="counter-warp">
-    <p>Vuex counter：{{ count }}</p>
-    <p>
-      <button @click="increment">+</button>
-      <button @click="decrement">-</button>
-    </p>
-
+    <p> 当前号码为：{{ count }}，请位于此号码前的司机</p>
+    <div class="text"> {{ count }}</div>
     <a href="/pages/index/main" class="home">去往首页</a>
+
   </div>
 </template>
 
 <script>
-// Use Vuex
-import store from './store'
+  // Use Vuex
+  import store from './store'
 
-export default {
-  computed: {
-    count () {
-      return store.state.count
-    }
-  },
-  methods: {
-    increment () {
-      store.commit('increment')
+  export default {
+    computed: {
+      count() {
+        return store.state.count
+      }
     },
-    decrement () {
-      store.commit('decrement')
+    methods: {
+      increment() {
+        store.commit('increment')
+      },
+      decrement() {
+        store.commit('decrement')
+      }
     }
   }
-}
 </script>
 
 <style>
-.counter-warp {
-  text-align: center;
-  margin-top: 100px;
-}
-.home {
-  display: inline-block;
-  margin: 100px auto;
-  padding: 5px 10px;
-  color: blue;
-  border: 1px solid blue;
-}
+  .counter-warp {
+    text-align: center;
+    margin-top: 100px;
+  }
+
+  .home {
+    display: inline-block;
+    margin: 100px auto;
+    padding: 5px 10px;
+    color: blue;
+    border: 1px solid blue;
+  }
+
+  .text {
+    font-size: 400rpx;
+    background-image: -webkit-linear-gradient(92deg, #f35626, #feab3a);
+    -webkit-text-fill-color: transparent;
+    -webkit-background-clip: text;
+   -webkit-animation:hue 30s infinite linear;
+  }
+
+  @keyframes hue {
+    0% {
+      -webkit-filter: hue-rotate(0deg);
+    }
+
+   100% {
+      -webkit-filter: hue-rotate(360deg);
+    }
+  }
 </style>
--- my-project/src/pages/counter/store.js
+++ my-project/src/pages/counter/store.js
@@ -7,7 +7,7 @@ Vue.use(Vuex)
 
 const store = new Vuex.Store({
   state: {
-    count: 0
+    count: 666
   },
   mutations: {
     increment: (state) => {
