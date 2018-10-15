c9b3e9164d98cb60a5b4c0f1adcf4a71964bfcc0
Administrator <397938635@qq.com>
2018-10-15 14:19:43 +0800

    Merge branch 'master' of https://github.com/mjc224000/tinympvue
    # Conflicts:
    #	my-project/project.config.json
    #	my-project/src/pages/counter/index.vue
    #	my-project/src/pages/counter/store.js
    #	server/package.json

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const model = require('./model');
-const WebSocket = require('ws');
 const orm = require('orm');
 let https = require('https');
 const {auth, setRole, showUsers} = require('./auth_manage');
@@ -10,12 +9,10 @@ let currentNumber = 0;
 let app = express();
 app.use(function (req, res, next) {
     res.setHeader("Access-Control-Allow-Origin", '*');
-
     setTimeout(next)
 })
 
 app.use(orm.express(model.url, {define: model.define}));
-
 
 app.get('/', function (req, res) {
     console.log(req.query);
@@ -24,7 +21,6 @@ app.get('/', function (req, res) {
 app.get('/setRole', setRole)
 app.get('/auth', auth);
 app.get('/api/users', showUsers);
-
 app.get('/update', function (req, res) {
     currentNumber = req.query.updateNumber;
     res.send('ok');
@@ -35,15 +31,7 @@ app.get('/currentNumber', function (req, res) {
 
 
 let server = https.createServer(options, app);
-const wss = new WebSocket.Server({server});
 
-wss.broadcast = function broadcast(data) {
-    wss.clients.forEach(function each(client) {
-        if (client.readyState === WebSocket.OPEN) {
-            client.send(data);
-        }
-    });
-};
 
 model.initDB().then(function () {
     server.listen(1443, 'localhost', function () {
