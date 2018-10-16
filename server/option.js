99c9b2b0ee29fa420a3051b6ff52e3a46ea09639
Administrator <397938635@qq.com>
2018-10-16 15:54:09 +0800

    Merge branch 'master' of https://github.com/mjc224000/tinympvue
                           # Conflicts:
                           #	my-project/project.config.json
                           #	my-project/src/pages/counter/index.vue
                           #	my-project/src/pages/counter/store.js
                           #	server/package.json

--- server/option.js
+++ server/option.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
-let privatekey = fs.readFileSync('./openssl/privatekey.pem'),
-    pc = fs.readFileSync('./openssl/certificate.pem');
+let key = fs.readFileSync('./openssl/215073622990525.key'),
+    cert = fs.readFileSync('./openssl/215073622990525.pem');
 module.exports = {
-    key: privatekey,
-    cert: pc,
-    appid:'wxe01f5e3d6ba2e2d7',
-    secret:"fe4922e0856d4c487703581dda62f0ee"
+    key: key,
+    cert: cert,
+    appid:'wx0b13931ed73bcf91',
+    secret:"8dbb7f28d9b71a9893e13f3769c1a9fc"
 }
