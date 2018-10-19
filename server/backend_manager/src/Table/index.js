1b4a036e4d5fb003b1a1537da7848b3cc09e85e4
Administrator <397938635@qq.com>
2018-10-19 17:43:26 +0800

    Merge branch 'master' of https://github.com/mjc224000/tinympvue
                           # Conflicts:
                           #	my-project/project.config.json
                           #	my-project/src/pages/counter/index.vue
                           #	my-project/src/pages/counter/store.js
                           #	server/package.json

--- server/backend_manager/src/Table/index.js
+++ server/backend_manager/src/Table/index.js
@@ -14,8 +14,8 @@ class BackEndTable extends Component {
             openid: '',
             isConfirmLoading: false,
             nickName: '',
-            msg: ''
-
+            msg: '',
+            modalType: ''
         }
     }
 
@@ -27,35 +27,32 @@ class BackEndTable extends Component {
             this.setState({list: res.data.list})
         }).catch((res) => console.log(res));
     }
-
-    closeAuth = () => {
-        this.setState({isAuthModalShow: false})
-    }
-
     closeConfirm = () => {
         this.setState(
             {
                 isConfirmModalShow: false,
                 nickName: '',
                 openid: '',
-                isConfirmLoading: false
+                isConfirmLoading: false,
+                modalType:''
             })
     }
 
-    grant = (record) => {
+    showModal = (record, modalType) => {
         let {openid, nickName} = record;
         this.setState(
             {
                 isConfirmModalShow: true,
                 openid, nickName,
-                msg:''
+                msg: '',
+                modalType
             });
     }
-
     confirm = () => {
-        let {openid} = this.state;
+        let {openid, modalType} = this.state;
         this.setState({isConfirmLoading: true});
-        axios.get('setRole', {params: {openid}}).then(res => {
+
+        axios.get(modalType === 'auth' ? 'setRole' : 'unAuth', {params: {openid}}).then(res => {
             if (res.data === 'ok') {
                 this.setState({msg: '设置成功'});
                 let that = this;
@@ -65,8 +62,26 @@ class BackEndTable extends Component {
             }
         })
     }
-
+    unAuth = () => {
+        let {openid} = this.state;
+        this.setState({isConfirmLoading: true});
+        axios.get('unAuth', {params: {openid}}).then(res => {
+            if (res.data === 'ok') {
+                this.setState({msg: '设置成功'});
+                let that = this;
+                setTimeout(function () {
+                    that.closeConfirm();
+                }, 500)
+            }
+        })
+    }
     render() {
+        let title,verb;
+        switch (this.state.modalType) {
+            case 'auth':title='授权';verb='给';break;
+            case 'unAuth':title='撤销';verb='撤销';break;
+            case 'delete':title='删除';verb='删除';break;
+        }
         return (
             <React.Fragment>
                 <Table dataSource={this.state.list}>
@@ -107,31 +122,34 @@ class BackEndTable extends Component {
                         title="角色"
                         dataIndex="role_roleid"
                         key="role_roleid"
-                        render={(text,record)=>{
-                            return (<span> {text===1?'管理员':'无'}</span>)
+                        render={(text, record) => {
+                            return (<span> {text === 1 ? '管理员' : '无'}</span>)
                         }}
                     />
                     <Column
                         title="Action"
                         key="action"
                         render={(text, record) => {
+                            console.log(record, 'record');
                             return (
                                 <span>
-          <Divider type="vertical"/>
-          <a href="javascript:;" onClick={() => this.grant(record)}>授权</a>
+                                    {record.role_roleid === 1 ? <a href="javascript:;"
+                                                                   onClick={() => this.showModal(record, 'unAuth')}>取消</a> : null}
+                                    <Divider type="vertical"/>
+          <a href="javascript:;" onClick={() => this.showModal(record, 'auth')}>授权</a>
         </span>
                             )
                         }}
                     />
                 </Table>
-                <Modal title="Title"
+                <Modal title={title}
                        visible={this.state.isConfirmModalShow}
                        onOk={this.confirm}
                        confirmLoading={this.state.isConfirmLoading}
                        onCancel={this.closeConfirm}
                 >
                     {
-                        this.state.msg === '' ? <p>授权确认，是否确定要给openid 为{this.state.openid},昵称为 <span
+                        this.state.msg === '' ? <p>授权确认，是否确定要{verb}openid 为{this.state.openid},昵称为 <span
                             style={{color: 'red'}}> {this.state.nickName}</span>
                             的用户授权
                         </p> : this.state.msg
