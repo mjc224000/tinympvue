6bb12ddd64e554dc37d5bac312ee28ef5ea10d23
39793 <397938635@qq.com>
2018-10-30 00:31:17 +0800

    backend manager

--- my-project/src/pages/messageList/index.vue
+++ my-project/src/pages/messageList/index.vue
@@ -1,75 +1,114 @@
 <template>
-    <div class="wrap">
-     <ul>
-       <li v-bind:class="{'_list':item.listClass,'wall':item.wallClass,}" v-for="item in list" :key="item.messageId">
-         <div class="top"><div class="left">{{item.title}}</div><div class="{right}">{{item.time}}></div></div>
-         <div class="content">
-         {{item.message}}
-       </div>
-         <hr/>
-       </li>
-     </ul>
-    </div>
+  <div class="wrap">
+    <ul>
+      <li  v-bind:class="{'alone':item.listClass,'encounter':item.wallClass,'together':item.princeClass,
+      'gallary':true,
+      'selected':item.selected
+      }"
+          v-for="item in list" :key="item.messageId"
+      v-on:click="tab"
+      >
+        <div style="width: 900rpx">
+          <div style="width:750rpx;height: 300rpx;float: left">
+            <div class="top">
+              <div class="left">{{item.title}}</div>
+              <div class="{right}">{{item.time}}></div>
+            </div>
+            <div class="content">
+              {{item.message}}
+            </div>
+            <hr/>
+          </div>
+          <div class="auth" style="height: 300rpx;float: left;width: 120rpx"></div>
+        </div>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
   import config from '@/config';
-    export default {
-        name: "index",
-      data:{
-          list:[ {messageId:1,message:'我是消息',title:'我是标题',time:'2018年十月24日'}]
-      },
-      created(){
-          let that=this;
-          wx.request({
-            url:config.messageList,
-            success(res){
-                      let list=res.data;
-              list.forEach((v,i)=>{
-                switch (i%2) {
-                  case 0:v.listClass=true;
-                    console.log(1);
-                    break;
-                  case 1:v.wallClass=true;break;
-                }
-              })
-          that.list=list;
-              console.log(res.data);
-            }
-          })
+
+  export default {
+    name: "index",
+    data: {
+      list: []
+    },
+    methods:{
+      tab(e){
+        console.log(e);
       }
     }
+    ,
+    created() {
+      let that = this;
+      wx.request({
+        url: config.messageList,
+        success(res) {
+          let list = res.data;
+          console.log(list);
+          list.forEach((v, i) => {
+            switch (i % 3) {
+              case 0:
+                v.listClass = true;
+                break;
+              case 1:
+                v.wallClass = true;
+                break;
+              case 2:
+                v.princeClass = true;
+                break;
+            }
+          })
+          that.list = list;
+          console.log(res.data);
+        }
+      })
+    }
+  }
 </script>
 
 <style scoped>
-  .top{
+  .top {
     display: flex;
     justify-content: space-around;
-    font-size: 24rpx;
+    font-size: 24 rpx;
   }
-  li{
+
+  li {
+    border-bottom: 1 rpx solid #aaa;
     height: 300rpx;
   }
-  li{
-    border-bottom: 1rpx solid #aaa;
-  }
-  .top div{
+.gallery{
+  height: 300rpx;
+  overflow: hidden;
+}
+  .top div {
     width: 50%;
-    color: #aaa;
   }
-  .right{
+
+  .right {
     text-align: right;
   }
-/*  li:nth-child(odd){
-    background:url("https://localhost/img/list.jpg") fixed;
-  }*/
- /* li:nth-child(even){
-    background: url("https://localhost/img/wall.jpg") fixed;
-  }*/
-  ._list{
-    background: url("https://localhost/img/list.jpg") fixed;
+
+  .alone {
+    background: url("https://localhost/img/alone.jpg") fixed;
+    color: wheat;
   }
-  .wall{
-    background: url("https://localhost/img/wall.jpg") fixed;
+
+  .encounter {
+    background: url("https://localhost/img/encounter.jpg") fixed;
+    background-position: -150 rpx;
+    color: black
+  }
+
+  .together {
+    background: url("https://localhost/img/together.jpg") fixed;
+    background-position: -400 rpx;
+    color: black;
+  }
+  .selected{
+    transform: translateX(-120rpx);
+    transition: 0.5s ease-in-out;
   }
 </style>
