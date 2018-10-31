a2442d7eb2ad522257227e458b818a3b0e4db8fb
Administrator <397938635@qq.com>
2018-10-31 17:38:30 +0800

    Merge branch 'master' of https://github.com/mjc224000/tinympvue
                           # Conflicts:
                           #	my-project/project.config.json
                           #	my-project/src/pages/counter/index.vue
                           #	my-project/src/pages/counter/store.js
                           #	server/package.json

--- server/msg_manager.js
+++ server/msg_manager.js
@@ -1,36 +1,55 @@
 function message(req, res) {
-    let messageModel=req.models.message
+    let messageModel = req.models.message
     let {message, title} = req.query;
-    messageModel.create({message, title,time: new Date()}, function (err) {
+    messageModel.create({message, title, time: new Date()}, function (err) {
         console.log(err);
         if (!err) res.send('ok');
- })
+    })
 }
-function deleteMessage(req,res){
-    let {message}=req.models.message;
-    let {messageId}=req.query;
+
+function putMessage(req, res) {
+    let messageModel = req.models.message;
+    let {messageId, message, title} = req.body;
+    messageModel.find({messageId}, function (err, doc) {
+        if (err) return;
+        let msg = doc[0];
+        if (msg) {
+            msg.message = message;
+            msg.title = title;
+            msg.save(function (err) {
+                if (err) return;
+                res.send('modified');
+            })
+        }
+    })
+}
+
+function deleteMessage(req, res) {
+    let {message} = req.models.message;
+    let {messageId} = req.query;
     message.find({messageId}).remove(function (err) {
-        if(!err){
+        if (!err) {
             res.send('ok');
         }
     })
 }
-function messageList(req,res){
-    let {message}=req.models;
-    message.find({},function (err,docs) {
-            if(!err){
 
-                docs=docs.map(v=>{
-                    v.time=new Date(v.time).toLocaleString();
-                    return v;
-                })
-                let c=docs[0];
-                for(var i=0;i<100;i++){
-                    docs.push(c);
-                }
-                res.json(docs);
+function messageList(req, res) {
+    let {message} = req.models;
+    message.find({}, function (err, docs) {
+        if (!err) {
+
+            docs = docs.map(v => {
+                v.time = new Date(v.time).toLocaleString();
+                return v;
+            })
+            let c = docs[0];
+            for (var i = 0; i < 100; i++) {
+                docs.push(c);
             }
-        })
+            res.json(docs);
+        }
+    })
 }
 
-module.exports = {message,messageList,deleteMessage};
+module.exports = {message, messageList, deleteMessage, putMessage};
